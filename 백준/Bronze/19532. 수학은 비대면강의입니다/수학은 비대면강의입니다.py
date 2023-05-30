f1145fcbbc5346a292446eef413d41666932f846
p-jiho <76985302+p-jiho@users.noreply.github.com>
2023-05-30 17:49:04 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -1,16 +1,4 @@
 a, b, c, d, e, f = map(int, input().split())
-first = [a, b, c]
-second = [d, e, f]
-if d != 0 and a != 0:
-    first = [d*first[i] for i in range(len(first))]
-    second = [a*second[i] for i in range(len(second))]
-    i = first[1] - second[1]
-    y = (first[2] - second[2])//i
-    x = (c - b*y)//a
-elif d != 0:
-    y = c//b
-    x = (f - e*y)//d
-elif a != 0:
-    y = e//f
-    x = (c - b*y)//a
-print(x, y)
+y = (c*d - f*a)//(b*d - e*a)
+x = (c*e - f*b)//(a*e - d*b)
+print(x,y)
