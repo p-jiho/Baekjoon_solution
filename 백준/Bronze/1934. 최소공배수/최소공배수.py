1deca7d7805ae22dddfeb24124297ba4db3a52ad
p-jiho <76985302+p-jiho@users.noreply.github.com>
2023-09-18 01:14:02 +0900

    [Bronze I] Title: 최소공배수, Time: 104 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.py
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -3,16 +3,10 @@ result = []
 for _ in range(case):
     first, second = map(int, input().split())
     second_k = []
-    if second % 2 == 0:
-        for i in range(second//2):
-            if second % (i+1) == 0:
-                second_k.append(i+1)
-                second_k.append(second//(i+1))
-    else:
-        for i in range(second//2+1):
-            if second % (i+1) == 0:
-                second_k.append(i+1)
-                second_k.append(second // (i + 1))
+    for i in range(int(second**(1/2))+1):
+        if second % (i+1) == 0:
+            second_k.append(i+1)
+            second_k.append(second//(i+1))
     second_k.sort()
     for s in second_k:
         if (first * s) % second == 0:
